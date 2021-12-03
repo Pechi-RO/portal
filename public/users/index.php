8605eefd8508491bf975d450126bc00a25cc0583
FranciscoRuizOrtega <71519403+FranciscoRuizOrtega@users.noreply.github.com>
2021-12-03 10:30:55 +0100

    a ver si ahora si

---
+++ public/users/index.php
@@ -0,0 +1,131 @@
+<?php
+session_start();
+require dirname(__DIR__, 2) . "/vendor/autoload.php";
+
+use Portal\Users;
+
+$imagen = (isset($_SESSION['user'])) ?
+    "../" . (new Users)->setUsername($_SESSION['user'])->getImg() : // /img/users/admin.jpg
+    "../img/users/invitado.png";
+
+$perfil = -1;
+if (isset($_SESSION['user'])) {
+    $perfil = (new Users)->setUsername($_SESSION['user'])->getPerfil();
+}
+
+$usuarios=(new Users)->readAll();
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Inicio usuario</title>
+
+
+
+
+
+</head>
+
+<body style="background-color:#ff9800">
+    <!-- NAVBAR ----------------------------->
+    <div class="py-2 d-flex" style="background-color:silver">
+        <div class="py-1">
+            &nbsp;<img src="<?php echo $imagen ?>" class="rounded-circle" height="40rem" width="40rem">
+        </div>
+        <div class="py-2">
+            &nbsp;<a href="../" style="text-decoration:none; color:white"><i class="fas fa-home"></i> INICIO</a>
+
+        </div>
+        <?php
+        if (!isset($_SESSION['user'])) {
+            echo <<<TXT
+            <div class="py-2">
+            &nbsp;&nbsp;<a href="register.php" style="text-decoration:none; color:blue">Registrarse</a>
+            </div>
+            <div class="py-2">
+            &nbsp;&nbsp;<a href="login.php" style="text-decoration:none; color:blue">Login</a>
+            </div>
+            TXT;
+        }
+
+        ?>
+        <div class="ms-auto">
+            <input class="form-control" type="text" disabled value="<?php echo (isset($_SESSION['user'])) ? $_SESSION['user'] : "Invitado"; ?>">
+        </div>
+        <div>
+            &nbsp;&nbsp;<a class="btn btn-info" href="<?php echo isset(($_SESSION['user'])) ? "cerrar.php" : "#" ?>"><i class="fas fa-sign-out-alt"></i></a>
+        </div>
+    </div>
+
+    </div>
+    <!-- FIN NAV BAR -->
+    <h3 class="text-center mt-2">Usuarios del Portal</h3>
+    <div class="container mt-2">
+        <?php
+        if ($perfil == 1) {
+            echo <<<TXT
+        <a href="register.php" class="btn btn-success"><i class="fas fa-user-plus"></i> Crear Usuario</a>
+        TXT;
+        }
+        ?>
+        <table class="table table-light table-striped mt-4">
+  <thead>
+    <tr>
+      <th scope="col">Información</th>
+      <th scope="col">Username</th>
+      <th scope="col">Email</th>
+      <th scope="col">Perfil</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php
+      foreach($usuarios as $item){
+        $color=($item->perfil==1)? "red" : "blue";
+        echo "<tr>";
+        echo "<th scope='row'><a href='dusuario.php?id={$item->id}' class='btn btn-info'><i class='fas fa-info'></i></th>";
+        echo "<td style='color:$color'>{$item->username}</td>";
+        echo "<td style='color:$color'>{$item->email}</td>";
+        echo "<td style='color:$color'>{$item->perfil}</td>";
+        echo "<td>";
+        if ($perfil==1){
+            //soy admin muestro todos los botones
+            echo <<<TXT
+            <form name="q" method="POST" action="buser.php">
+            <input type="hidden" name="id" value="{$item->id}" />
+            <a href="eusuario.php?id={$item->id}" class="btn btn-warning">
+            <i class="fas fa-edit"></i>
+            </a>
+            <button type="submit" class="btn btn-danger" onclick="return confirm('Seguro que quieres borrar este usuario?')"><i class="fas fa-trash"></i></button>
+            TXT;
+        }elseif($perfil==0){
+            //soy usuario normal solo boton editar
+            if ($_SESSION['user']==$item->username){
+                echo "<a href='eusuario.php?id={$item->id}' class='btn btn-warning'>";
+                echo "<i class='fas fa-edit'><i/></a>";
+            }else{
+                echo "<button class='btn btn-danger' disabled><i class='fas fa-exclamation-circle'></i>";
+            }
+
+        }
+    
+        echo "</td>";
+        echo "</tr>";
+    }
+   ?>
+  </tbody>
+</table>
+    </div>
+</body>
+
+</html>
---
+++ public/users/register.php
@@ -0,0 +1,222 @@
+<?php
+session_start();
+require dirname(__DIR__, 2) . "/vendor/autoload.php";
+
+use Portal\Users;
+
+$perfil = -1;
+$error=false;
+
+if (isset($_SESSION['user'])) {
+    $perfil = (new Users)->setUsername($_SESSION)->getPerfil();
+}
+if ($perfil == 0) {
+    //header("Location:index.php");
+}
+
+if(isset($_POST['registrar'])){
+    $username=trim($_POST['username']);
+    $email=trim($_POST['email']);
+    $password=trim($_POST['password']);
+    $img=$_POST['img'];
+    $perfil=$_POST['perfil'];
+
+    //comprobamos que los campos no estén vacíos
+    if($username==""){
+        $error=true;
+        $_SESSION['username']="no puedes introdcir el campo username vacio";
+    }
+    if ($email=""){
+        $error=true;
+        $_SESSION['email']="no puedes introducir el campo email vacio";
+    }
+    if ($password=""){
+        $error=true;
+        $_SESSION['password']="no puedes introducir el campo password vacio";
+    }
+
+    //comprobamos que no hay errores al usar valores ya existentes en la base de datos
+    $userprev=(new Users)->readAll()->username;
+    $emailprev=(new Users)->readAll()->email;
+    
+    if(in_array($username,$userprev)){
+        $error=true;
+        $_SESSION['username']='El username elegido ya existe en la base de datos';
+    }
+    if(in_array($email,$emailprev)){
+        $error=true;
+        $_SESSION['email']="El email elegido ya existe en la base de datos";
+    }
+
+    //comprobamos el campo img
+    //falta hacer la funcion para comprobar los tipos mime
+    //es lo de is imagen
+
+    if (!isset($_FILES['img']['tmp_name'])){
+        //no se ha elegido imagen asi que le asignamos una dependiendo de si se ha creado admin o user
+        //cada uno tiene una por defecto
+
+        if ($perfil==1){
+            $img=(new Users)->setImg("../img/users/admin.jpg");
+
+        }elseif($perfil==0){
+            $img=(new Users)->setImg("../img/users/users.png");
+        }
+
+    }
+
+    //vemos si ha habido errores
+    if ($error==false){
+        //no hay errores, guardamos los datos
+        (new Users)->setUsername($username)
+        //la pass hay que codificarla pero no recuerdo como
+        ->setPassword($password)
+        ->setEmail($email)
+        ->setPerfil($perfil)
+        //hay que retocar el setter de img de la clase usuario para que se guarde ennuestra carpeta de imagenes
+        ->setImg($img)
+        ->create();
+        $_SESSION['correcto']="Usuario guardado con exito";
+
+    }else{
+        //hay errores, pintamos la pagina y los mostramos
+    }
+
+
+}else{
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Registrar Usuario</title>
+
+
+
+
+
+</head>
+
+<body style="background-color:silver">
+    <h5 class="text-center mt-4">Registrar Usuario</h5>
+    <div class="container mt-2">
+<?php
+        if(isset($_SESSION['correcto'])){
+            echo <<<TXT
+            <div class="alert alert-success" role="alert" value='{$_SESSION["correcto"]}'>
+            
+            </div>
+        TXT;
+        }
+
+?>
+
+
+        <div class="bg-success p-4 text-white rounded shadow-lg m-auto" style="width:35rem">
+            <form name="cautor" action="<?php echo $_SERVER['PHP_SELF'] ?>" method='POST' enctype="multipart/form-data">
+
+                <div class="mb-3">
+                    <label for="n" class="form-label">Nombre Usuario</label>
+                    <input type="text" class="form-control" id="n" placeholder="Username" name="username" required>
+                <?php
+                if (isset($_SESSION['username'])){
+                    echo <<<TXT
+            <div class="alert alert-danger" role="alert" value='{$_SESSION["username"]}'>
+            
+            </div>
+        TXT;
+                }
+
+                ?>
+                
+                </div>
+                <div class="mb-3">
+                    <label for="a" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="a" placeholder="Correo" name="email" required>
+                
+                    <?php
+                if (isset($_SESSION['email'])){
+                    echo <<<TXT
+            <div class="alert alert-danger" role="alert" value='{$_SESSION["email"]}'>
+            
+            </div>
+        TXT;
+                }
+
+                ?>
+                
+                </div>
+
+                
+
+                <div class="mb-3">
+                    <label for="p" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="p" placeholder="Contraseña" name="password" required>
+                
+                    <?php
+                if (isset($_SESSION['password'])){
+                    echo <<<TXT
+            <div class="alert alert-danger" role="alert" value='{$_SESSION["password"]}'>
+            
+            </div>
+        TXT;
+                }
+
+                ?>
+                
+                </div>
+
+                <div class="mb-3">
+                    <label for="f" class="form-label">Imagen</label>
+                    <input class="form-control" type="file" id="f" name="img">
+                    <?php
+                if (isset($_SESSION['img'])){
+                    echo <<<TXT
+            <div class="alert alert-danger" role="alert" value='{$_SESSION["img"]}'>
+            
+            </div>
+        TXT;
+                }
+
+                ?>
+
+                </div>
+
+                <?php
+
+                if ($perfil == 1) {
+                    //NOTA
+                    //EL echo <<<TXT y el codigo html debe estar al mismo nivel tal y como esta ahora,
+                    //el cierre debe estar al mismo nivel o más atrás,no después sino da fallo
+                    echo <<< TXT
+                    <div class="mb-3">
+                    <label for="p" class="form-label">Perfil</label>
+                    <select class="form-control" name="perfil">
+                    <option value="1">Administrador</option>
+                    <option value="0" selected>Usuario</option>
+                TXT;
+                }
+                ?>
+
+                <div>
+                    <button type='submit' name="registrar" class="btn btn-info"><i class="fas fa-save"></i> Registrar</button>
+                    <button type="reset" class="btn btn-warning"><i class="fas fa-broom"></i> Limpiar</button>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+</body>
+
+</html><?php } ?>
